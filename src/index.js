381b6d8c92ee0a5aeb9cf350c50e8847e491dae9
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-05-10 03:48:04 +0200

    first-step

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
+
+
+// import { searchPhoto } from "./js/searchPhoto";
+
+
+
+console.log(searchPhoto)
+
+
+
+
+
+
+
+
+// webformatURL - link do małego obrazka.
+// largeImageURL - link do dużego obrazka.
+// tags - wiersz z opisem obrazka. Będzie pasować do atrybutu alt.
+// likes - liczba “lajków”.
+// views - liczba wyświetleń.
+// comments - liczba komentarzy.
+// downloads - liczba pobrań.
+
+////////.................//////////////////
+
+//  "Sorry, there are no images matching your search query. Please try again."
+
+
+ //////////////..................////////////////
+
+
+
+
+
+
+
+
+// Utwórz frontend aplikacji wyszukiwania i przeglądania obrazków według słów kluczowych. //
+
+
+
+
+
 // bibl.////
 // axios   notiflix
 
@@ -6,3 +49,8 @@
 
 
 //API Pixabay
+
+
+
+
+
---
+++ src/js/searchPhoto.js
@@ -0,0 +1,36 @@
+// API request //
+
+import axios from "axios";
+
+const valExp = 'dog'
+
+const inputEl = document.querySelector('input[name = "searchQuery"]')
+
+const searchPhoto = async () => {
+    const API_URL = 'https://pixabay.com/api/';
+    const KEY = '36240096-2eba6952fadc15ce6318a051b';
+    const userValue = valExp;
+    const param = 'imagine_type=photo&orientation=horizontal&safesearch=true';
+
+    return await axios.get(`${API_URL}?key=${KEY}&q=${userValue}&${param}`)
+        // .then(response => response.data)
+}
+
+export {searchPhoto}
+
+
+
+
+
+
+//parametrs api//
+// Key - 36240096-2eba6952fadc15ce6318a051b
+// q - tresc uzytkownika
+// image_type - photo
+// orientation - horizontal
+// safesearch - true
+/////............./////////////
+ //const API_URL = 'https://pixabay.com/api/?key=36240096-2eba6952fadc15ce6318a051b&q=yellow+flowers&image_type=photo';
+ //${API_URL}?key=${KEY}&q=${userValue}&${param}
+
+
